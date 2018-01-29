9fc6a34dc5801dd45ce5c4368fbce5efb8e11ca0
alan.cao <zongying.cao@hpe.com>
2018-01-28 22:27:04 -0500

    fix bug in test

--- init_service_code/tests/yamlgen_test.py
+++ init_service_code/tests/yamlgen_test.py
@@ -58,7 +58,7 @@ def convertToModel_test():
     import demjson
     from models.ansible.yamlgen import convertToModel
     json = demjson.decode("[{'deployConfig': {'image_tag': '1.0', 'volume': '/working', 'description': null, 'entrypoint': 'node index.js', '_id': '5a446071f521b50001971f98', 'image': 'alancao/node_server_image', 'port': 8082, 'name': 'edgesvr2', 'restart': false, 'instanceType': 'microService', 'target': null, 'volumes': [{'container': '/working'}], 'awsSetting': null, 'recreate': false}}]")
-    result = convertToModel(json)
+    result = convertToModel(json, "./")
 
     assert_that(result.services[0].name).is_equal_to("edgesvr2")
     assert_that(len(result.services[0].volumes)).is_equal_to(1)
