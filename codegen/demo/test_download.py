5e93e4f478e737dad16fe78c474f186622572774
alan cao <zongying.cao@dxc.com>
2018-07-24 22:00:23 -0400

    refactor files
    Signed-off-by: alan cao <zongying.cao@dxc.com>

--- codegen/demo/test_download.py
+++ codegen/demo/test_download.py
@@ -1,8 +1,9 @@
 from codegen import Download
+from assertpy import assert_that
 
 def test_download():
     url="http://http://130.175.64.9:9008/_api/interface_service"
     download = Download()
     download.getUserName()
     download.getPwd()
-    download.getJson(url)
+    assert_that(download.getJson(url)).contins('name')
