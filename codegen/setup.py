e962ea4faee4acb398c33be4b68ed79c1476855e
alan cao <zongying.cao@dxc.com>
2018-07-28 03:40:31 -0400

    add scripts
    Signed-off-by: alan cao <zongying.cao@dxc.com>

--- codegen/setup.py
+++ codegen/setup.py
@@ -6,7 +6,7 @@ requires = []
 
 setup(
     name = name,
-    version = '0.0.1',
+    version = '0.0.2',
     scripts = ["scripts/codegen"]
     author = 'Zongying Cao',
     author_email = 'zongying.cao@dxc.com',
