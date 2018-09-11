15f02b46c076996cf71e7bb665e0b9fed140538d
alan cao <zongying.cao@dxc.com>
2018-09-11 01:45:10 -0400

    refactor the code
    Signed-off-by: alan cao <zongying.cao@dxc.com>

--- codegen/codegen/app.py
+++ codegen/codegen/app.py
@@ -3,6 +3,9 @@ from .download import getJson
 from codegenhelper import put_folder, debug
 
 def run(root, url, username = None, password = None):
+    def gen_code(app_data, project_folder):
+        nodejs_codegen.run(app_data, project_folder)
+    
     (lambda folder_path: \
-     [nodejs_codegen.run(debug(app_data, "app_data"), \
+     [gen_code(debug(app_data, "app_data"), \
           put_folder(app_data["deployConfig"]["instanceName"], folder_path)) for app_data in getJson(url, username, password) ])(put_folder(root))
