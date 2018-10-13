29541f1b840d06d10ae31398a5f04db0aa10eab2
Ubuntu <caozon@dev.tmljhwvwekuuzmg1zw4txphbgc.cx.internal.cloudapp.net>
2018-10-13 07:10:11 +0000

    apply changes in nodejs-codegen
    Signed-off-by: Ubuntu <caozon@dev.tmljhwvwekuuzmg1zw4txphbgc.cx.internal.cloudapp.net>

--- codegen/setup.py
+++ codegen/setup.py
@@ -2,11 +2,11 @@
 from setuptools import setup, find_packages
 name = "md_codegen"
 
-requires = ['nodejs_codegen>=0.3.6', 'codegenhelper>=0.0.12']
+requires = ['nodejs_codegen>=0.3.7', 'codegenhelper>=0.0.12']
 
 setup(
     name = name,
-    version = '0.2.6',
+    version = '0.2.7',
     scripts = ["scripts/codegen"],
     author = 'Zongying Cao',
     author_email = 'zongying.cao@dxc.com',
