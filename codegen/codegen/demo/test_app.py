fdf4dc000080ac5d3db82e9292af24e6422735f4
Ubuntu <caozon@dev.tmljhwvwekuuzmg1zw4txphbgc.cx.internal.cloudapp.net>
2018-11-12 02:34:51 +0000

    improve the download url
    Signed-off-by: Ubuntu <caozon@dev.tmljhwvwekuuzmg1zw4txphbgc.cx.internal.cloudapp.net>

--- codegen/codegen/demo/test_app.py
+++ codegen/codegen/demo/test_app.py
@@ -3,10 +3,11 @@ from assertpy import assert_that
 import os
 
 def test_run():
-    url = "http://130.175.64.9:9008/_api/interface_service/interface_service"
+    url = "http://104.46.103.208:9008"
     username = "alan1"
     password = "123"
     root = "./test"
-    run(root, url, username, password)
+    project_name = "test1"
+    run(root, url, project_name, username, password)
 
     assert_that(os.path.exists(root)).is_true()
--- codegen/codegen/download.py
+++ codegen/codegen/download.py
@@ -10,20 +10,22 @@ def get_input(name):
         raise Exception("no {} is supplied".format(name))
     return result
 
-def get_login_url(url):
-    return (lambda result:"{scheme}://{netloc}/auth/login".format(scheme = debug(result, "result").scheme, netloc = result.netloc))(urlparse(url))
+def get_login_url(host_url):
+    return (lambda result:"{scheme}://{netloc}/auth/login".format(scheme = debug(result, "result").scheme, netloc = result.netloc))(urlparse(host_url))
 
 def getToken(login_url, name, pwd):
     return (lambda result:\
             result.content.decode(result.encoding))\
             (requests.post(login_url, json={"name": name, "pwd": pwd}))
 
-def get_json(url, name, token):
-    return debug(requests.get(url, headers={"name": name, "token": token}).text, "get_json_result")
+def get_json(host_url, name, token):
+    return debug(requests.get(host_url, headers={"name": name, "token": token}).text, "get_json_result")
 
-def getJson(url, userName=None, password=None):
+def build_download_url(host_url, user_name, project_name):
+    return host_url + "/_api/interfacedef_get_service/query/{project}/interface_entries".format(project="$$".join([user_name, project_name]))
+def getJson(host_url, project_name, userName=None, password=None):
     return demjson.decode(\
                           (lambda name, pwd:\
-            get_json(url, name, getToken(get_login_url(url), name, pwd))) \
+            get_json(build_download_url(host_url, name, project_name), name, getToken(get_login_url(host_url), name, pwd))) \
             (userName or get_input("username"), password or getpass("please input password:")) \
     )
