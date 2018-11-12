046351711e55a76165440b7a0532b19cf52ccfb1
Ubuntu <caozon@dev.tmljhwvwekuuzmg1zw4txphbgc.cx.internal.cloudapp.net>
2018-11-12 02:43:25 +0000

    increase to 0.2.8
    Signed-off-by: Ubuntu <caozon@dev.tmljhwvwekuuzmg1zw4txphbgc.cx.internal.cloudapp.net>

--- codegen/setup.py
+++ codegen/setup.py
@@ -6,7 +6,7 @@ requires = ['nodejs_codegen>=0.3.7', 'codegenhelper>=0.0.12']
 
 setup(
     name = name,
-    version = '0.2.7',
+    version = '0.2.8',
     scripts = ["scripts/codegen"],
     author = 'Zongying Cao',
     author_email = 'zongying.cao@dxc.com',
