52f39d9f0953c454979e996fb7a7212298f862fd
Ubuntu <caozon@dev.tmljhwvwekuuzmg1zw4txphbgc.cx.internal.cloudapp.net>
2018-12-01 04:20:06 +0000

    refactor for test_download.py
    Signed-off-by: Ubuntu <caozon@dev.tmljhwvwekuuzmg1zw4txphbgc.cx.internal.cloudapp.net>

--- codegen/codegen/demo/test_download.py
+++
@@ -1,8 +0,0 @@
-from codegen.download import getJson
-from assertpy import assert_that
-
-def test_download():
-    url="http://104.46.103.208:9008"
-
-    assert_that(getJson(url, "test1", 'alan1', '123')[0]).contains('deployConfig')
-
--- codegen/codegen/tests/test_download.py
+++ codegen/codegen/tests/test_download.py
@@ -5,3 +5,11 @@ def test_get_login_url():
     url = "http://130.123.2.5:8990/_api/interface_service/interface_service"
     
     assert_that(get_login_url(url)).is_equal_to("http://130.123.2.5:8990/auth/login")
+
+
+def test_download():
+    url="http://104.46.103.208:9008"
+
+    assert_that(getJson(url, "test1", 'alan1', '123')[0]).contains('deployConfig')
+
+
